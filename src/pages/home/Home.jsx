86f73c1734f33226b65cbe8405e5900996091012
shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2024-12-10 19:05:42 +0500

    finish

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container">
+      <h2>Home</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+        maiores nulla ea distinctio, illum animi quia. Odio laboriosam laborum
+        illum excepturi nihil unde ab asperiores placeat atque amet est velit
+        quis numquam, harum facilis corrupti culpa praesentium nesciunt soluta
+        quae illo totam! Pariatur porro repellat officia odit quia alias
+        dolores?
+      </p>
 
-export default Home
+      <p>Sign up ga kirish uchun Login page ga kiring yoki Register</p>
+    </div>
+  );
+};
+
+export default Home;
